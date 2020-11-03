12150ee7baecbf9891413d415e2aaeddc51dcdbe
SDMesaAssignments <60202182+SDMesaAssignments@users.noreply.github.com>
2020-11-03 10:58:34 -0800

    Made unsigned char --> char in upperAndChar function declaration

--- lab_code/apex_code.h
+++ lab_code/apex_code.h
@@ -7,6 +7,6 @@
 #define APEXLAB_APEX_CODE_H
 
 int getNumberTiles(double heightInFt, double widthInFt, int tileSizeInInches);
-bool upperCharAnd(unsigned char char1, unsigned char char2);
+bool upperCharAnd(char char1, char char2);
 
 #endif //APEXLAB_APEX_CODE_H
